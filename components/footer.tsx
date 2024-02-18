a1986bdffd08c52a0dca51f0a05379c5b78ba8d7
Hossein Askari <hosseinaskari.it@gmail.com>
2024-02-18 12:25:52 +0330

    Add all my files

---
+++ components/footer.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <footer className="bg-white boarder-t">
+      <div className="mx-auto py-10">
+        <p className="text-center text-xs text-black">
+          &copy; 2023 FakeStoreNameA, Inc. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+};
+export default Footer;
---
+++ components/preview-modal.tsx
@@ -0,0 +1,25 @@
+"use client";
+import usePreviewModal from "@/hooks/use-preview-modal";
+import Modal from "@/components/ui/modal";
+import Gallery from "@/components/gallery";
+import Info from "@/components/info";
+
+const PreviewModal = () => {
+  const previewModal = usePreviewModal();
+  const product = usePreviewModal((state) => state.data);
+
+  if (!product) {
+    return null;
+  }
+  return (
+    <Modal open={previewModal.isOpen} onClose={previewModal.onClose}>
+      <div className="sm:col-span-4 lg:col-span-5">
+        <Gallery images={product.images} />
+      </div>
+      <div className="sm:col-span-8 lg:col-span-7">
+        <Info data={product} />
+      </div>
+    </Modal>
+  );
+};
+export default PreviewModal;
